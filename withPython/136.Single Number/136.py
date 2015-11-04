09fb187cc42e95b07e38404153738e08930a4720
Wyn <yingwen@92@163.com>
2015-11-04 21:27:41 +0800

    my solution

---
+++ withPython/136.Single Number/136.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        r = 0
+        for i in nums:
+            r = r ^ i
+            
+        return r
